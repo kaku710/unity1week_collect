2bcfa30eb48737660678618be85572dd52b8e338
Takeshi Kaku <t.kaku710@gmail.com>
2019-07-03 03:37:47 +0900

    MVP作成

---
+++ Assets/Scripts/InGamePresenter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class InGamePresenter : MonoBehaviour
+{
+   [SerializeField] private InGameView view;
+    private void Start(){
+        LoadGame();
+        Bind();
+    }
+
+    private void LoadGame(){
+        GameManager.Instance.SetMoney(0);
+        GameManager.Instance.SetPersonProductivity(0);
+        GameManager.Instance.SetSecondsProductivity(0);
+    }
+
+    private void Bind(){
+        GameManager.Instance.Money
+            .Subscribe(view.OnMoneyChanged)
+            .AddTo(gameObject);
+        GameManager.Instance.PersonProductivity
+            .Subscribe(view.OnPersonProductivityChanged)
+            .AddTo(gameObject);
+        GameManager.Instance.SecondsProductivity
+            .Subscribe(view.OnSecondsProductivityText)
+            .AddTo(gameObject);
+    }
+}
---
+++ Assets/Scripts/InGameView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameView : MonoBehaviour {
+    [SerializeField] private Text
+    moneyText,
+    personProductivityText,
+    secondsProductivityText;
+
+    public void OnMoneyChanged (int money) {
+        moneyText.text = "¥" + money.ToString ();
+    }
+
+    public void OnPersonProductivityChanged (int person) {
+        personProductivityText.text = person.ToString ();
+    }
+
+    public void OnSecondsProductivityText (float seconds) {
+        secondsProductivityText.text = seconds.ToString ();
+    }
+}
