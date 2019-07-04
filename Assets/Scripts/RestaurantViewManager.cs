297e234d744d1cc2c5bfcd8ea1cc86df357514e5
Takeshi Kaku <t.kaku710@gmail.com>
2019-07-05 02:34:25 +0900

    RestaurantViewManager作成 あとDOTweenインポートした

---
+++ Assets/Scripts/RestaurantViewManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestaurantViewManager : MonoBehaviour {
+    [SerializeField] private GameObject mainCam;
+    private Vector3 defaultCameraPos = new Vector3 (4.2f, 3.3f, 1.2f);
+    private Vector3 secondCameraPos = new Vector3 (6f, 5f, 4.6f);
+    private void Start () {
+        mainCam.transform.position = defaultCameraPos;
+    }
+}
