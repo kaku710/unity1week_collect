fd3448c18952cdc2333c81e0f5519c6af74a6a4c
Takeshi Kaku <t.kaku710@gmail.com>
2019-07-05 03:20:03 +0900

    CustomerSpawnerクラス作成等

---
+++ Assets/Scripts/Customer/CustomerSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform seatTransform;
+    [SerializeField] private GameObject customer;
+
+    private void Start(){
+        Instantiate(customer,seatTransform.position,Quaternion.identity);
+    }
+}
