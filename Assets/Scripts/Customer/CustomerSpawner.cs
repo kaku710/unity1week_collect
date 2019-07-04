6d6924e87b3472c6298bacbae0b23e6b76e2412f
Takeshi Kaku <t.kaku710@gmail.com>
2019-07-05 03:58:59 +0900

    客自動生成途中

--- Assets/Scripts/Customer/CustomerSpawner.cs
+++ Assets/Scripts/Customer/CustomerSpawner.cs
@@ -5,8 +5,21 @@ using UnityEngine;
 public class CustomerSpawner : MonoBehaviour {
     [SerializeField] private Transform[] seatTransforms;
     [SerializeField] private GameObject customer;
+    private float spawnTimer;
 
     private void Start () {
+        
+    }
+
+    private void Update(){
+        spawnTimer += Time.deltaTime;
+        if(spawnTimer >= StatusManager.Instance.SecondsProductivity.Value){
+            SpawnCustomer();
+            spawnTimer = 0;
+        }
+    }
+
+    private void SpawnCustomer(){
         int rndNumber = Random.Range (0, StatusManager.Instance.SeatCount.Value);
         float angleY = (rndNumber % 4) * 90;
         Instantiate (customer, seatTransforms[rndNumber].position, Quaternion.Euler (0, angleY, 0));
