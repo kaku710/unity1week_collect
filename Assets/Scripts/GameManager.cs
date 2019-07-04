1ecc2135d0af47a3ae7c0654648ed0301f7031a6
Takeshi Kaku <t.kaku710@gmail.com>
2019-07-05 02:22:11 +0900

    レストランの配置一旦完了

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,19 +4,6 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : SingletonMonoBehaviour<GameManager> {
-    [SerializeField] private Customer customer;
-    private float spawnCustomerTimer;
-    [SerializeField] private Transform customerParentTransform;
+        
 
-    private void Update () {
-        spawnCustomerTimer += Time.deltaTime;
-        if (spawnCustomerTimer >= StatusManager.Instance.SecondsProductivity.Value) {
-            SpawnCustomer ();
-            spawnCustomerTimer = 0;
-        }
-    }
-
-    private void SpawnCustomer () {
-        Instantiate (customer, customerParentTransform);
-    }
 }
