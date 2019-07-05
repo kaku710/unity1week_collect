336da998bd13039abe400c5c5bd67c082a4cc51c
Takeshi Kaku <t.kaku710@gmail.com>
2019-07-06 02:28:26 +0900

    GameManager準備

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -5,5 +5,29 @@ using UnityEngine.UI;
 
 public class GameManager : SingletonMonoBehaviour<GameManager> {
         
+    public enum GameState{
+        TITLE,
+        GAME,
+        RESULT
+    }
+    private GameState currentGameState;
+
+    public void SetCurrentState(GameState state){
+        currentGameState = state;
+        OnGameStateChanged(currentGameState);
+    }
+
+    private void OnGameStateChanged(GameState state){
+        switch(state){
+            case GameState.GAME:
+                break;
+            case GameState.RESULT:
+                break;
+            case GameState.TITLE:
+                break;
+            default:
+                break;
+        }
+    }
 
 }
