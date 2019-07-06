c74704d9c1974eab2a824e32de978314f5e29e6b
Takeshi Kaku <t.kaku710@gmail.com>
2019-07-06 16:59:22 +0900

    車生成されるようにした

---
+++ Assets/Scripts/Car/Car.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.position -= transform.forward * 3f * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider col){
+        if(col.gameObject.tag == "EndLine"){
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Car/CarSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour {
+    [SerializeField] private float angle;
+    private float spawnTimer;
+    private float interval;
+
+    private void Start(){
+        interval = Random.Range (2f, 4f);
+    }
+
+    private void Update () {
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer >= interval) {
+            SpawnCar ();
+            interval = Random.Range (5f, 10f);
+            spawnTimer = 0;
+        }
+    }
+
+    private void SpawnCar () {
+        var num = Random.Range (0, 3);
+        var car = Resources.Load ("Cars/Car" + num);
+        Instantiate (car, transform.position, Quaternion.Euler (0, angle, 0));
+    }
+}
